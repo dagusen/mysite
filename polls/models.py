1d257c847c63f823a6010321254ea3ddf2ff1347
Louis <louisaleksieb.dagusen@gmail.com>
2017-07-14 14:28:34 -0400

    Authentication

--- polls/models.py
+++ polls/models.py
@@ -1,24 +1,26 @@
 import datetime
+
+
 from django.db import models
-<<<<<<< HEAD
 from django.utils.encoding import python_2_unicode_compatible
 
 from django.db import models
 from django.utils import timezone
-=======
-from django.utils.encoding import datetime from django.utils import time zone
->>>>>>> 6808b972767adbcfaa7adb7806dde2b4031aca45
+
+from django.utils.encoding import datetime 
+from django.utils import timezone
+
 # Create your models here.
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     def __str__(self):
-<<<<<<< HEAD
+
     	return self.question_text
-=======
+
         return self.question_text
->>>>>>> 6808b972767adbcfaa7adb7806dde2b4031aca45
+
     def was_published_recently(self):
     	return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
