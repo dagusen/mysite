4165f46ad413e07cd83d8d7a84d14d72fb6c7572
Louis <louisaleksieb.dagusen@gmail.com>
2017-07-14 15:28:28 -0400

    raise Http404 if the object doesn’t exist. Django provides a shortcut. Here’s the detail() view

--- polls/views.py
+++ polls/views.py
@@ -4,8 +4,13 @@ from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
 
+from django.shortcuts import get_object_or_404, render
 from .models import Question
 # Create your views here.
+
+def detail(request, question_id):
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'polls/detail.html', {'question':question})
 
 def detail(request, question_id):
 	try:
